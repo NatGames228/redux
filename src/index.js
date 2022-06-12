f4d223a948d49bf07e31da333e967c4093fe6240
NatGames <salavat-iskhakov@mail.ru>
2022-06-12 19:17:54 +0300

    add react

--- src/index.js
+++ src/index.js
@@ -3,37 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
-import * as actions from './action';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
 
 const store = createStore(reducer);
-
-const { dispatch, subscribe, getState } = store;
-
-const update = () => {
-  document.getElementById('counter').textContent = getState().value;
-}
-
-subscribe(update);
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args));
-// }
-
-const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
-
-document.getElementById('inc').addEventListener('click', inc);
-
-document.getElementById('dec').addEventListener('click', dec);
-
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-})
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
     <React.StrictMode>
-      <></>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   );
