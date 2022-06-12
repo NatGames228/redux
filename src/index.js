1dce2ab0169562a1f63622f53e955a081fff03d7
NatGames <salavat-iskhakov@mail.ru>
2022-06-12 18:18:41 +0300

    redux start

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createStore, bindActionCreators } from 'redux';
+
+import reducer from './reducer';
+import * as actions from './action';
+
+const store = createStore(reducer);
+
+const { dispatch, subscribe, getState } = store;
+
+const update = () => {
+  document.getElementById('counter').textContent = getState().value;
+}
+
+subscribe(update);
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// }
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+
+document.getElementById('dec').addEventListener('click', dec);
+
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  rnd(value);
+})
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(
+    <React.StrictMode>
+      <></>
+    </React.StrictMode>,
+  );
